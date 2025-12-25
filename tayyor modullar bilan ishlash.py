f5c167e71e9c82fb336b9e115be00d51a557c0cc
baxtli-ona <mamatovagulchiroy492@gmail.com>
2025-12-25 12:06:53 +0500

    yangi mavzu qo'shdim

---
+++ tayyor modullar bilan ishlash.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 23 15:23:53 2025
+
+@author: acer
+"""
+
+import datetime as dt
+
+# hozir=dt.datetime.now()
+# # print(hozir)
+bugun=dt.date.today()
+# # vaqt_hozir=hozir.time()
+# # ramazon=dt.date(2026,2,17)
+# futbol=dt.datetime(2025,12,26,22,50,10)
+# vaqt=futbol-hozir
+# sekundlar=vaqt.seconds
+# minutlar=int(sekundlar/60)
+# soat=int(minutlar/60)
+# # print(f"Bugungi sana: {bugun}."
+# #       f"Ertangi sana: {ertaga}")
+# # print(vaqt_hozir)
+# # farq=ramazon-bugun
+# # print(f"Ramazon oyi boshlanishiga {farq.days} kun qoldi")
+
+# print(f"Futbol boshlanishiga {vaqt.days} kun {soat} soat qoldi")
+
+# import math
+# # PI=math.pi
+# # print(f"PI ning qiymati {PI} ga teng")
+# # E=math.e
+# # print(f"e ning qiymati {E} ga teng")
+# print(math.degrees(math.pi/2))
+# log=math.log(5)
+# log_1=math.log10(100)
+# print(log, log_1)
+# x=50
+# print(round(x))
+# print(math.pow(x, 10))
+# import re
+# from uzwords import words
+
+# andoza="^б...а$"
+# matches=[]
+
+# for word in words:
+#     if re.match(andoza, word):
+#         matches.append(word)
+# print(matches)
+# andoza='^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$'
+# msg="Yangi parol kiriting"
+# msg+='(Kamida 8 ta iborat kamida bitta lotin bosh harf, 1 ta kichik harf,'
+# msg+="1 ta son va 1 ta maxsus belgi bo'lishi kerak):"
+# while True:
+#     password=input(msg)
+#     if re.match(andoza,password):
+#         print("Maxfiy so'z qabulqilindi" )
+#         break
+#     else:
+#         print("Maxfiy so'z qabul qilinmadi. Takror boshqa so'z kiriting")
+
+# for sana in range(bugun,10,2): 
+#     print(sana)
+print(bugun)
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ xatolarni_to'girla.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 23 15:23:53 2025
+
+@author: acer
+"""
+
+# son=float(input("Juft son kiriting: "))
+# if son%2!=0:
+#     print("Bu son juft emas.")
+# else:
+#     print("Rahmat!")
+
+# yosh = int(input("Yoshingiz nechida?"))
+
+# if yosh<=4 or yosh>=60:
+#     narh = 0
+# elif yosh < 18:
+#     narh = 10000
+# else:
+#     narh = 20000
+    
+# print(f"Chipta {narh} so'm")
+
+# x = float(input("Birinchi sonni kiriting: "))
+# y = float(input("Ikkinchi sonni kiriting: "))
+# if x==y:
+#     print(f"{x}={y}")
+# elif x<y:
+#     print(f"{x}<{y}")
+# else:
+#     print(f"{x}>{y}")
+
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#                'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+# savat=[]
+
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+
+# if savat:
+#     for mahsulot in savat:
+#         if mahsulot in mahsulotlar:
+#             print(f"Do'konimizda {mahsulot} bor")
+#         else:
+#             print(f"Do'konimizda {mahsulot} yo'q")
+# else:
+#     print("Savatingiz bo'sh")       
+
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#                'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulot)
+#     else:
+#         mavjud_emas.append(mahsulot)
+
+# if mavjud_emas:
+#   print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#   for mahsulot in mavjud_emas:
+#       print(mahsulot)
+# else:
+#     print("Siz so'ragan barcha mahsulotlar do'konimizda bor") 
+#     for mahsulot in bor_mahsulotlar:
+#         print(mahsulot)
+   
+print("y/(x-5) ni o'rniga son qo'yib ishlaymiz")  
+savol=True 
+while savol: 
+    x=int(input("Son kiriting x="))
+    y=int(input("Son kiriting y="))  
+    try:
+        print("y/(x-5)=",y/(x-5))
+    except ZeroDivisionError:
+        print("o ga bo'lish munkin emas")
+        continue
+    savol=int(input("Yana son kiritamizmi:ha(1), yo'q(0): "))
+    
+    
+    
+    
+    
+    
+    
+    
+    
